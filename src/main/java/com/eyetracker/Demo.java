d7f6a2aaa5a3a75cb2bffadb6311ab511921b7b0
Vukkum Sai Prakash <vukkumsp@gmail.com>
2018-03-18 12:18:07 +0530

    Able to detect the Face from Webcam. Print the image in a specified folder only if a face is detected.

--- src/main/java/com/eyetracker/Demo.java
+++ src/main/java/com/eyetracker/Demo.java
@@ -7,10 +7,22 @@
 package com.eyetracker;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
+import org.openimaj.image.processing.face.detection.DetectedFace;
+import org.openimaj.image.processing.face.detection.HaarCascadeDetector;
 import org.openimaj.video.VideoDisplay;
 import org.openimaj.video.VideoDisplayListener;
 import org.openimaj.video.capture.VideoCapture;
@@ -19,11 +31,13 @@ import org.openimaj.video.capture.VideoCapture;
  *
  * @author Sai Prakash
  */
-public class Demo {
+public class Demo{
     static String outputFolderPath = "C:\\Users\\DELL\\Desktop\\images\\";
     
-    public static void main(String args[]) throws IOException{
-        gettingImageFromWebcam();
+    public static void main(String args[]) throws IOException, InterruptedException{
+//        gettingImageFromWebcam();
+//        recognizeFaceInImage();
+        readingImageAndDetectingFace();
     }
     
     public static void gettingImageFromWebcam() throws IOException{
@@ -36,5 +50,58 @@ public class Demo {
             number++;
         }
         webcam.close();
+    }
+    
+    public static void recognizeFaceInImage() throws IOException{
+        HaarCascadeDetector detector = new HaarCascadeDetector();
+        List<DetectedFace> faces = null;
+        File file = new File(outputFolderPath+"hello0"+".png");
+        BufferedImage image = ImageIO.read(file);
+        detector.detectFaces(ImageUtilities.createFImage(image));
+       faces = detector.detectFaces(ImageUtilities.createFImage(image));
+       if (faces == null) {
+        System.out.println("No faces found in the captured image");
+        return;
+       }
+       Iterator <DetectedFace> dfi = faces.iterator();
+       while (dfi.hasNext()) {
+        DetectedFace face = dfi.next();
+        System.out.println("FOUND at: "+face.getBounds());
+       }
+    }
+    
+    public static void readingImageAndDetectingFace() throws IOException, InterruptedException{
+        HaarCascadeDetector detector = new HaarCascadeDetector();
+        List<DetectedFace> faces = null;
+        
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        while(true){
+            
+            faces = null;
+            BufferedImage image = webcam.getImage();
+            faces = detector.detectFaces(ImageUtilities.createFImage(image));
+            if (faces == null) {
+                System.out.println("No faces found in the captured image");
+                continue;
+            }
+            Iterator <DetectedFace> dfi = faces.iterator();
+            if(dfi.hasNext()){
+                date = calendar.getTime();
+                String dateString = date.toString().replaceAll(" ","").replaceAll(":","");
+                ImageIO.write(image, "PNG", new File((outputFolderPath+dateString+".png")));
+            }
+            while (dfi.hasNext()) {
+                DetectedFace face = dfi.next();
+                System.out.println("FOUND at: "+face.getBounds());
+            }
+            //sleep after every second to avoid/delay Date collision
+            Thread.sleep(1000);
+        }
+//        webcam.close();
+        
+        
     }
 }
