b17b9f722ba67e38d1cf69216dc1c2d11f73943a
Vukkum Sai Prakash <vukkumsp@gmail.com>
2018-03-18 11:27:03 +0530

    Getting 10 images continuesly.

--- src/main/java/com/eyetracker/Demo.java
+++ src/main/java/com/eyetracker/Demo.java
@@ -10,18 +10,31 @@ import com.github.sarxos.webcam.Webcam;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import org.openimaj.image.MBFImage;
+import org.openimaj.video.VideoDisplay;
+import org.openimaj.video.VideoDisplayListener;
+import org.openimaj.video.capture.VideoCapture;
 
 /**
  *
  * @author Sai Prakash
  */
 public class Demo {
-    static String outputFolderPath = "C:\\Users\\DELL\\Desktop\\";
+    static String outputFolderPath = "C:\\Users\\DELL\\Desktop\\images\\";
     
     public static void main(String args[]) throws IOException{
-        
+        gettingImageFromWebcam();
+    }
+    
+    public static void gettingImageFromWebcam() throws IOException{
         Webcam webcam = Webcam.getDefault();
         webcam.open();
-        ImageIO.write(webcam.getImage(), "PNG", new File(outputFolderPath+"hello-world.png"));
+        int number = 0;
+        String fileName = "hello";
+        while(number < 10){
+            ImageIO.write(webcam.getImage(), "PNG", new File(outputFolderPath+fileName+number+".png"));
+            number++;
+        }
+        webcam.close();
     }
 }
