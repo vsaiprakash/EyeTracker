19d6b84b41449e84350081c72db73625fc0d9465
Vukkum Sai Prakash <vukkumsp@gmail.com>
2018-03-18 10:06:06 +0530

    Testing webcam access - Successful

---
+++ src/main/java/com/eyetracker/Demo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.eyetracker;
+
+import com.github.sarxos.webcam.Webcam;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Sai Prakash
+ */
+public class Demo {
+    static String outputFolderPath = "C:\\Users\\DELL\\Desktop\\";
+    
+    public static void main(String args[]) throws IOException{
+        
+        Webcam webcam = Webcam.getDefault();
+        webcam.open();
+        ImageIO.write(webcam.getImage(), "PNG", new File(outputFolderPath+"hello-world.png"));
+    }
+}
